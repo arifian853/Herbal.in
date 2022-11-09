3b29ffdf76196f6ad3304b706a30b7977fce9597
unknown <arifiansaputra43@gmail.com>
2022-11-09 20:56:04 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Selamat Datang di Herbal.in !
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Selamat Datang
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
